d656de5325c5a443216ac03e0b5f257c32dccc06
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2024-01-08 19:25:45 +0000

    Update API key

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -195,7 +195,7 @@ const config = {
       appId: 'ZBH2T1WJQD',
 
       // Public API key: it is safe to commit it
-      apiKey: 'fb61697222d5d23d13d387540b5515a4',
+      apiKey: '62f58247ec020d11cd9d655e22b5fc6d',
 
       indexName: 'luke-geek',
 
