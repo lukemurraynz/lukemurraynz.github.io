5d0f5241563f47708f153550c6284bcf484ad639
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2024-01-22 00:56:45 +0000

    Add About page to navigation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,6 +110,11 @@ const config = {
             position: "left",
           },
           {
+            label: "About",
+            href: "/about/",
+            position: "left",
+          },
+          {
             href: "https://www.youtube.com/channel/UCUG3TKDTAZz4UXshGrjBBoQ",
             position: "right",
             className: "navbar-icon",
